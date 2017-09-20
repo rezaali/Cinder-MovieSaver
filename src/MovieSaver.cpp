055faf199ce7a99937d6e0b755ac02c004b8ef56
rezaali <syed.reza.ali@gmail.com>
2017-09-20 09:39:58 -0700

    removed logging & updated copyWindowSurface methods

--- src/MovieSaver.cpp
+++ src/MovieSaver.cpp
@@ -29,9 +29,9 @@ void MovieSaver::save( const ci::fs::path &path, const std::string &filename, co
 	mCurrentFrame = 0;
 	mCurrentTime = 0.0f;
 	mProgress = 0.0f;
-
+    
 	auto format = qtime::MovieWriter::Format().codec( qtime::MovieWriter::JPEG ).jpegQuality( 1.0 ).fileType( qtime::MovieWriter::QUICK_TIME_MOVIE ).averageBitsPerSecond( 1000000000 ).defaultFrameDuration( 1.0 / 60.0 );
-	mMovieExporter = qtime::MovieWriter::create( addPath( addPath( path, filename ), extension, "." ), ci::app::toPixels( mWindowRef->getWidth() ), ci::app::toPixels( mWindowRef->getHeight() ), format );
+    mMovieExporter = qtime::MovieWriter::create( addPath( addPath( path, filename ), extension, "." ), mWindowRef->toPixels( mWindowRef->getWidth() ), mWindowRef->toPixels( mWindowRef->getHeight() ), format );
 
 	mRecording = true;
 }
@@ -40,12 +40,10 @@ void MovieSaver::update()
 {
 	mCurrentTime = float( mCurrentFrame ) / float( mTotalFrames );
 	if( mRecording ) {
-		CI_LOG_D( "TIME: " + toString( mCurrentTime ) );
-		CI_LOG_D( "FRAME: " + toString( mCurrentFrame ) );
 		int frameNumber = mCurrentFrame;
 		if( frameNumber <= mTotalFrames ) {
-			mProgress = float( frameNumber ) / float( mTotalFrames );
-			mMovieExporter->addFrame( ci::app::copyWindowSurface() );
+			mProgress = float( frameNumber ) / float( mTotalFrames );			
+            mMovieExporter->addFrame( mWindowRef->getRenderer()->copyWindowSurface( Area( 0, 0, mWindowRef->toPixels( mWindowRef->getWidth() ), mWindowRef->toPixels( mWindowRef->getHeight() ) ), mWindowRef->toPixels( mWindowRef->getHeight() ) ) );
 		}
 		if( frameNumber == mTotalFrames ) {
 			mMovieExporter->finish();
